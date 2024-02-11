32a1df64439c7afc9a6ac934fc0981060ff247ec
Grzegorz <gkopacki92@gmail.com>
2024-02-11 14:36:19 +0100

    created Question board

---
+++ src/utils/Questions.js
@@ -0,0 +1,7 @@
+export const Question = [
+	{ question: "Co jest głównym składnikiem risotto?", answerOne: "ryż", answerTwo:"pomidory", answerThree:"ser", answerFour:"oregano" },
+	{ question: "Co jest głównym składnikiem ziemskiej atmosfery?", answerOne: "tlen", answerTwo:"węgiel", answerThree:"azot", answerFour:"para wodna" },
+	{ question: "Hokkaido to jedna z popularnych odmian?", answerOne: "jabłek", answerTwo:"papryki", answerThree:"dyni", answerFour:"cebuli" },
+	{ question: "Jaką dyscypline uprawiał Tiger Woods?", answerOne: "piłkę nożną", answerTwo:"tenis", answerThree:"łucznictwo", answerFour:"golf" },
+	{ question: "Którą epoką historyczną zajmuje się mediewistyka?", answerOne: "starożytnością", answerTwo:"średniowieczem", answerThree:"oświeceniem", answerFour:"prehistorią" },
+];

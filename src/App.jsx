9f494c1d3049b812f33bf2d3917ff7418b8a092b
Grzegorz <gkopacki92@gmail.com>
2024-02-13 12:18:34 +0100

    added handle for created new Questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 import { useState } from "react";
 import { Question } from "./components/Question/Question";
-import { Questions } from "./utils/Questions";
+// import { Questions } from "./utils/Questions";
 import styles from "./App.module.css";
 import { ResultSummary } from "./components/ResultSummary/ResultSummary";
 import { Form } from "./components/Form/Form";
 
 const correctAnswers = ["ryż", "azot", "dyni", "golf", "średniowieczem"];
+const Questions = [
+	{
+		question: "Co jest głównym składnikiem risotto?",
+		answerOne: "ryż",
+		answerTwo: "pomidory",
+		answerThree: "ser",
+		answerFour: "oregano",
+		id: 1,
+	},
+	{
+		question: "Co jest głównym składnikiem ziemskiej atmosfery?",
+		answerOne: "tlen",
+		answerTwo: "węgiel",
+		answerThree: "azot",
+		answerFour: "para wodna",
+		id: 2,
+	},
+	{
+		question: "Hokkaido to jedna z popularnych odmian?",
+		answerOne: "jabłek",
+		answerTwo: "papryki",
+		answerThree: "dyni",
+		answerFour: "cebuli",
+		id: 3,
+	},
+	{
+		question: "Jaką dyscypline uprawiał Tiger Woods?",
+		answerOne: "piłkę nożną",
+		answerTwo: "tenis",
+		answerThree: "łucznictwo",
+		answerFour: "golf",
+		id: 4,
+	},
+	{
+		question: "Którą epoką historyczną zajmuje się mediewistyka?",
+		answerOne: "starożytnością",
+		answerTwo: "średniowieczem",
+		answerThree: "oświeceniem",
+		answerFour: "prehistorią",
+		id: 5,
+	},
+];
 
 function App() {
 	const [changeQuestion, setChangeQuestion] = useState(0);
@@ -13,19 +55,19 @@ function App() {
 	const [showSummary, setShowSummary] = useState(false);
 	const [userAnswers, setUserAnswers] = useState([]);
 	const [userScore, setUserScore] = useState(0);
-	// const [newQuestions, setNewQuestions] = useState(Questions);
-
+	const [newQuestions, setNewQuestions] = useState(Questions);
+	const [newCorrectAnswers, setNewCorrectAnswers] = useState(correctAnswers);
 	function nextQuestion(e) {
 		const userAnswer = e.target.textContent;
 		setUserAnswers((prevUserAnswer) => [...prevUserAnswer, userAnswer]);
 		setChangeQuestion((prevQuestion) => prevQuestion + 1);
 		setHandleHeading((prevHeading) => prevHeading + 1);
 
-		if (handleHeading === 5) {
+		if (handleHeading === newQuestions.length) {
 			setShowSummary(true);
 		}
 
-		const isCorrectAnswer = userAnswer === correctAnswers[changeQuestion];
+		const isCorrectAnswer = userAnswer === newCorrectAnswers[changeQuestion];
 
 		if (isCorrectAnswer) {
 			setUserScore((prevScore) => prevScore + 1);
@@ -58,24 +100,52 @@ function App() {
 			<div className={styles.container}>
 				{!showSummary && (
 					<Question
-						heading={`pytanie ${handleHeading} z 5`}
-						question={Questions[changeQuestion].question}
-						answerOne={Questions[changeQuestion].answerOne}
-						answerTwo={Questions[changeQuestion].answerTwo}
-						answerThree={Questions[changeQuestion].answerThree}
-						answerFour={Questions[changeQuestion].answerFour}
+						heading={`pytanie ${handleHeading}`}
+						question={newQuestions[changeQuestion].question}
+						answerOne={newQuestions[changeQuestion].answerOne}
+						answerTwo={newQuestions[changeQuestion].answerTwo}
+						answerThree={newQuestions[changeQuestion].answerThree}
+						answerFour={newQuestions[changeQuestion].answerFour}
 						onClick={nextQuestion}
 					/>
 				)}
 				{showSummary && (
 					<ResultSummary
 						userAnswers={userAnswers}
-						correctAnswers={correctAnswers}
+						correctAnswers={newCorrectAnswers}
 						score={userScore}
 					/>
 				)}
 			</div>
-			<Form />
+			<Form
+				onQuestionsSubmit={(
+					question,
+					answerOne,
+					answerTwo,
+					answerThree,
+					answerFour,
+					correctAnswer
+				) => {
+					setNewQuestions((prevQuestion) => {
+						return [
+							...prevQuestion,
+							{
+								question,
+								answerOne,
+								answerTwo,
+								answerThree,
+								answerFour,
+								id: prevQuestion.length + 1,
+							},
+						];
+					});
+
+					setNewCorrectAnswers((prevCorrectAnswer) => [
+						correctAnswer,
+						...prevCorrectAnswer,
+					]);
+				}}
+			/>
 		</>
 	);
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,45 +1,128 @@
+import { useState } from "react";
 import styles from "./Form.module.css";
 
-export function Form() {
+export function Form({ onQuestionsSubmit }) {
+	const [questionValue, setQuestionValue] = useState("");
+	const [answerOneValue, setAnswerOneValue] = useState("");
+	const [answerTwoValue, setAnswerTwoValue] = useState("");
+	const [answerThreeValue, setAnswerThreeValue] = useState("");
+	const [answerFourValue, setAnswerFourValue] = useState("");
+	const [correctAnswerValue, setCorrectAnswerValue] = useState("");
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		const question = questionValue;
+		const answerOne = answerOneValue;
+		const answerTwo = answerTwoValue;
+		const answerThree = answerThreeValue;
+		const answerFour = answerFourValue;
+		const correctAnswer = correctAnswerValue;
+
+		setQuestionValue("");
+		setAnswerOneValue("");
+		setAnswerTwoValue("");
+		setAnswerThreeValue("");
+		setAnswerFourValue("");
+		setCorrectAnswerValue("");
+
+		onQuestionsSubmit(
+			question,
+			answerOne,
+			answerTwo,
+			answerThree,
+			answerFour,
+			correctAnswer
+		);
+	}
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={handleSubmit}>
 			<div className={styles.formBox}>
 				<div>
 					<label htmlFor="question">Pytanie:</label>
 				</div>
-				<input type="text" name="question" />
+				<input
+					type="text"
+					name="question"
+					id="question"
+					value={questionValue}
+					onChange={(event) => {
+						setQuestionValue(event.target.value);
+					}}
+				/>
 			</div>
 			<div className={styles.formBox}>
 				<div>
 					<label htmlFor="answerOne">Odpowiedź nr 1:</label>
 				</div>
-				<input type="text" name="answerOne" />
+				<input
+					type="text"
+					name="answerOne"
+					id="answerOne"
+					value={answerOneValue}
+					onChange={(event) => {
+						setAnswerOneValue(event.target.value);
+					}}
+				/>
 			</div>
 			<div className={styles.formBox}>
 				<div>
 					<label htmlFor="answerTwo">Odpowiedź nr 2:</label>
 				</div>
-				<input type="text" name="answerTwo" />
+				<input
+					type="text"
+					name="answerTwo"
+					id="answerTwo"
+					value={answerTwoValue}
+					onChange={(event) => {
+						setAnswerTwoValue(event.target.value);
+					}}
+				/>
 			</div>
 			<div className={styles.formBox}>
 				<div>
 					<label htmlFor="answerThree">Odpowiedź nr 3:</label>
 				</div>
-				<input type="text" name="answerThree" />
+				<input
+					type="text"
+					name="answerThree"
+					id="answerThree"
+					value={answerThreeValue}
+					onChange={(event) => {
+						setAnswerThreeValue(event.target.value);
+					}}
+				/>
 			</div>
 			<div className={styles.formBox}>
 				<div>
 					<label htmlFor="answerFour">Odpowiedź nr 4:</label>
 				</div>
-				<input type="text" name="answerFour" />
+				<input
+					type="text"
+					name="answerFour"
+					id="answerFour"
+					value={answerFourValue}
+					onChange={(event) => {
+						setAnswerFourValue(event.target.value);
+					}}
+				/>
 			</div>
 			<div className={styles.formBox}>
 				<div>
 					<label htmlFor="correctAnswer">Poprawna odpowiedź:</label>
 				</div>
-				<input type="text" name="correctAnswer" />
+				<input
+					type="text"
+					name="correctAnswer"
+					id="correctAnswer"
+					value={correctAnswerValue}
+					onChange={(event) => {
+						setCorrectAnswerValue(event.target.value);
+					}}
+				/>
 			</div>
-            <button className={styles.saveForm}>Zapisz</button>
+			<button className={styles.saveForm} type="submit">
+				Zapisz
+			</button>
 		</form>
 	);
 }
